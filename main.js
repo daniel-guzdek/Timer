63b1c9e815e5348a3b4577c7d9e5605a12c5a173
Daniel Guzdek <daniel.guzdek.86@gmail.com>
2021-05-19 16:22:24 +0200

    Created files: style.css main.js

---
+++ main.js
@@ -0,0 +1,41 @@
+const btnStartPause = document.querySelector('.start');
+const btnReset = document.querySelector('.reset');
+const divTimer = document.querySelector('.timer');
+
+let active = 0;
+let onOff;
+let seconds = 0;
+
+function onOffTimer() {
+  active++;
+  if (active === 1) {
+    btnStartPause.textContent = "Pause";
+    start();
+  } else if (active === 2) {
+    btnStartPause.textContent = "Start";
+    active = 0;
+    pause();
+  }
+}
+
+function start() {
+  onOff = setInterval(timer, 10);
+}
+
+function pause() {
+  clearInterval(onOff);
+}
+
+function timer() {
+  seconds++
+  divTimer.textContent = (seconds / 100).toFixed(2);
+}
+
+function reset() {
+  seconds = 0;
+  divTimer.textContent = "---";
+  clearInterval(onOff);
+}
+
+btnStartPause.addEventListener('click', onOffTimer);
+btnReset.addEventListener('click', reset);
